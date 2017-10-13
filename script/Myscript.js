3cbdbe5d597df8ad99bd8a34a93662392da5c1d6
Rafael Lino <rafael.linosp@gmail.com>
2017-10-12 22:38:36 -0300

    atualiza 12/10

--- script/Myscript.js
+++ script/Myscript.js
@@ -1,3 +1,14 @@
- function imgNotFound(img){
+
+function resetSenha() {
+    var senha = document.getElementsByName('senha');
+    for(var i=0;i<senha.length ;i++){
+        senha[i].value="";
+    }
+}
+
+
+function imgNotFound(img){
  	img.src = "img/icon/noImage.jpg";
  }
+
+
