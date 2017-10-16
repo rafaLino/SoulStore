24eab3522b8aa03d81fad9472351a1fb70d7661f
Rafael Lino <rafael.linosp@gmail.com>
2017-10-16 01:41:31 -0200

    continuando...

--- script/Myscript.js
+++ script/Myscript.js
@@ -9,4 +9,15 @@ function imgNotFound(img){
  	img.src = "img/icon/noImage.jpg";
  }
 
-
+ function showContador(){
+    var cont=0,box,interval;
+    box = document.getElementsByClassName('box-cart')[0];
+    box.style.display ="block";
+    interval = setInterval(function () {
+             cont++;
+        if (cont == 5){
+            box.style.display="none";
+            clearInterval(interval);
+               }
+           },1000);
+       }
