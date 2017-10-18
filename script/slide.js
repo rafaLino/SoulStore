775043164be7ea0511c22fc3c32ece7d19b96b4a
Rafael Lino <rafael.linosp@gmail.com>
2017-10-18 16:13:54 -0200

    slides

---
+++ script/slide.js
@@ -0,0 +1,32 @@
+
+    var proximo,anterior,index=1,imagens,id;
+    window.onload = automatic;
+
+        function automatic(){
+            imagens = document.getElementsByClassName("imagens");
+            proximo = document.getElementById("proximo");
+            anterior = document.getElementById("anterior");
+            id = setTimeout(automatic,4000);
+            proximo.onclick = function(){
+                    automatic();
+                    clearTimeout(id);
+            };
+            anterior.onclick = function() {
+                index-=2;
+                clearTimeout(id);
+                automatic();
+            };
+            console.log(index);
+            if(index > imagens.length){
+                index=1;
+            }
+            if(index < 1){
+                index = imagens.length;
+            }
+            for (var i = 0; i < imagens.length; i++) {
+                imagens[i].style.display = "none";
+            }
+            imagens[index-1].style.display="block";
+            if(!isNaN(id)){ index++;}
+        }
+
