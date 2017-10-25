b65f03bf36bf1ea6abafb36f98a2d4870170a848
Rafael Lino <rafael.linosp@gmail.com>
2017-10-25 15:56:00 -0200

    comentado

--- src/model/DB.php
+++ src/model/DB.php
@@ -65,11 +65,12 @@ class DB
 
 
         private function getPropertie($string):string{
-            while(!feof($this->file)){
-                $line = fgets($this->file);
-                if(substr_count($line,$string)>0){ break;}
+            while(!feof($this->file)){ //enquanto existir linhas no doc
+                $line = fgets($this->file);//pega a linha
+                if(substr_count($line,$string)>0){ break;} //se na linha existir a string passada como parametro
             }
-            if (!is_bool(strpos($line, ':')))
+            if (!is_bool(strpos($line, ':'))) //se encontrar ":" na linha
+                //retorna string tomando a posição do ":" como inicio até o final da linha,retirando espaços
                 return trim(substr($line, strpos($line,":")+strlen(":")));
     }
 
